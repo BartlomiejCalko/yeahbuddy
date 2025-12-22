78d640c140cfb1fbe7a625702ca349bb54a8a358
barto <bartlomiej.calko@gmail.com>
2025-12-22 21:17:44 +0100

    Initial Commit

---
+++ yeahbuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yeahbuddy
+//
+//  Created by Bartlomiej Calko on 22/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yeahbuddy/yeahbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  yeahbuddyApp.swift
+//  yeahbuddy
+//
+//  Created by Bartlomiej Calko on 22/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct yeahbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
